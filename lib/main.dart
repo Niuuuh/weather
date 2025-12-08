89e653d2be4b10a243a6648caa6309e35a741312
Leon <leon.martin@stud.hs-hannover.de>
2025-12-08 09:16:37 +0100

    Add todos

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 
+// todo: add weather icons
+// todo: add current location
+// todo: add localizations
+// todo: style settings
+
 void main() {
   runApp(
     ProviderScope(
